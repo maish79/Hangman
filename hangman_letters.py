cbcae9a31a99d94710fdef85bb9dad4c44f9b51e
maish79 <maishsupa@yahoo.com>
2022-11-04 11:31:52 +0000

    add variables and hangman_letters

---
+++ hangman_letters.py
@@ -0,0 +1,4 @@
+word_list =[
+    'monkey','mankind','python','toad','gazelle','impala','jaguar','elephant','lizard',
+    'donkey','snake','bufallo','rhino','anaconda','hippo','parrot','eagle'
+    ]
--- run.py
+++ run.py
@@ -65,3 +65,13 @@ logo = '''
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                     __/ |                      
                    |___/    '''
+
+import random
+import hangman_letters
+ 
+ #variables
+ a = set("abcdefghijklmnopqrstuvwxyz")
+ lives = 6
+ guessed = " "
+ end_of_game = False
+ words = random.choice(hangman_letters.word_list)
