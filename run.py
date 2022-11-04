f9b07811f571e92f1f8ee7011d8b00878d6fe3d0
maish79 <maishsupa@yahoo.com>
2022-11-04 11:38:02 +0000

    add greeting and name inputs

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ logo = '''
 | |                                            
 | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
 | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
+| | | | (_| | | | | (_| | | | | | | (_| | | | 
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                     __/ |                      
                    |___/    '''
@@ -70,8 +70,21 @@ import random
 import hangman_letters
  
  #variables
- a = set("abcdefghijklmnopqrstuvwxyz")
- lives = 6
- guessed = " "
- end_of_game = False
- words = random.choice(hangman_letters.word_list)
+a = set("abcdefghijklmnopqrstuvwxyz")
+lives = 6
+guessed = " "
+end_of_game = False
+words = random.choice(hangman_letters.word_list)
+
+print(logo)
+
+name = input("Enter your name:\n")
+print("welcome",name,"!")
+print("You have 6 attempts to guess the word!")
+
+blank = []
+for i in word:
+    blank.append("_")
+
+while not end_of_game:
+    guess = input("Guess a letter:\n").lower()
